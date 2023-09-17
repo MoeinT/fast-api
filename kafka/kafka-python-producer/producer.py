ae11a970d39ab64ecd0a69416dbdd34d2dfba9b4
Torabi Mohammadmoein <mohammadmoein.torabi@volvo.com>
2023-09-17 20:34:40 +0200

    broker external listerner and py

--- kafka/kafka-python-producer/producer.py
+++ kafka/kafka-python-producer/producer.py
@@ -1,15 +1,40 @@
 from confluent_kafka import Producer
+import csv
+import time
 
 # Producer configuration
 producer_config = {
-    'bootstrap.servers': 'localhost:9092',  # Change to the appropriate broker(s)
+    'bootstrap.servers': '127.0.0.1:9092',  # Change to the appropriate broker(s)
 }
 
-# Create a Kafka producer
 producer = Producer(producer_config)
 
-# Produce a message to a Kafka topic
-producer.produce('my-topic', key='key', value='Hello, Kafka!')
+# Open and read the CSV file
+with open("../data/indexProcessed.csv", newline='') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    
+    # Iterate through each row in the CSV file
+    for row in csv_reader:
+        try:
+            message = ",".join(row)  # Convert the CSV row to a string
+            producer.produce("my-topic", key='key', value=message)
+            producer.flush()  # Flush to ensure the message is sent immediately
+        except Exception as e:
+            print(f"An error occurred while sending a message: {e}")
+        
+        # Wait for 10 seconds before sending the next message
+        time.sleep(10)
 
-# Wait for any outstanding messages to be delivered and delivery reports to be received
-producer.flush()
+
+
+
+
+
+# # Create a Kafka producer
+# producer = Producer(producer_config)
+
+# # Produce a message to a Kafka topic
+# producer.produce('my-topic', key='key', value="Farima is my love.")
+
+# # Wait for any outstanding messages to be delivered and delivery reports to be received
+# producer.flush()
